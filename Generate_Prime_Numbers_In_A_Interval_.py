36811f22d8832aae2a017311ebb76ad78d48e1b0
DEVARASAIGANESH29 <119840486+DEVARASAIGANESH29@users.noreply.github.com>
2023-05-28 16:22:46 +0530

    def p(x):     c = 0     for i in range(1,x+1):         if x%i == 0:             c+=1     if c == 2:         return True     else:         return False a = int(input()) b = int(input()) for i in range(a,b):     if p(i) is True:         print(i)

---
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -0,0 +1,14 @@
+def p(x):
+    c = 0
+    for i in range(1,x+1):
+        if x%i == 0:
+            c+=1
+    if c == 2:
+        return True
+    else:
+        return False
+a = int(input())
+b = int(input())
+for i in range(a,b):
+    if p(i) is True:
+        print(i)
