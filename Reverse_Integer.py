31b2e3fa83202ab8f5d8e0cd9c5303ea87e15d2e
DEVARASAIGANESH29 <119840486+DEVARASAIGANESH29@users.noreply.github.com>
2023-05-28 16:49:30 +0530

    a = input() b = a[::-1] k=[] for i in b:     k.append(i) for j in range(0,len(k)):     if k[0]=="0":         k.remove(k[0])     elif k[-1]=="-":         k.remove(k[-1])         k.insert(0,"-") for l in k:     print(l,end='')

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,13 +1,15 @@
-n = input()
-x = n[::-1]
-a=[]
-for i in x:
-    a.append(i)
-for j in range(0,len(a)):
-    if a[0]=='0':
-        a.remove(a[0])
-    elif a[-1]=='-':
-        a.remove(a[-1])
-        a.insert(0,'-')
-for l in a:
-    print(l,end="")
+a = input()
+b = a[::-1]
+k=[]
+for i in b:
+    k.append(i)
+for j in range(0,len(k)):
+    if k[0]=="0":
+        k.remove(k[0])
+    elif k[-1]=="-":
+        k.remove(k[-1])
+        k.insert(0,"-")
+for l in k:
+    print(l,end='')
+    
+    
