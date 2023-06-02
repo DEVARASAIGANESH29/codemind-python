ac365d829615a11520ba0a36e7d8a52f4ec26c0c
DEVARASAIGANESH29 <119840486+DEVARASAIGANESH29@users.noreply.github.com>
2023-06-02 15:39:02 +0530

    # Python3 code to demonstrate # checking a number is palindrome # using str() + string slicing
    # initializing number
    test_number = input()
    # using str() + string slicing
    # for checking a number is palindrome
    res = str(test_number) == str(test_number)[::-1]
    
    # printing result
    print (str(res))

--- Palindrome.py
+++ Palindrome.py
@@ -1,6 +1,12 @@
-n = input()
-a = n[::-1]
-if n==a:
-    print("True")
-else:
-    print("False")
+# Python3 code to demonstrate
+# checking a number is palindrome
+# using str() + string slicing
+
+# initializing number
+test_number = input()
+# using str() + string slicing
+# for checking a number is palindrome
+res = str(test_number) == str(test_number)[::-1]
+
+# printing result
+print (str(res))
