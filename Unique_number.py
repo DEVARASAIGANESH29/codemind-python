ce0eed45891c01d98430aae8e652bd4c69009e22
DEVARASAIGANESH29 <119840486+DEVARASAIGANESH29@users.noreply.github.com>
2023-06-02 15:18:02 +0530

    a = input() c=0 for i in a:     if a.count(i)==1:         c+=1 if c==len(a):     print("Unique Number") else:     print("Not Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,10 +1,9 @@
-n = int(input())
-a = str(n)
+a = input()
 c=0
 for i in a:
     if a.count(i)==1:
-        c +=1
-if len(a)==c:
+        c+=1
+if c==len(a):
     print("Unique Number")
 else:
     print("Not Unique Number")
